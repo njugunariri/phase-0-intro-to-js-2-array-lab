cde09c4a1d7225cf7461559df70d6f94dd95bf63
njuguna <rihanna8njuguna@gmail.com>
2024-04-04 09:31:03 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const tutorials = [
+    "what does the this keyword mean?",
+    "What is the Constructor OO pattern?",
+    "implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What is NaN and how Can we Check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable State and Pure Functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?",
+  ];
+
+  function titleCased() {
+    return tutorials.map(function(tutorial) {
+        let letters = tutorial.split('');
+        let capitalizedLetters = letters.map(function(letter) {
+            return letter.charAt(0).toUpperCase() + letter.slice(1);
+        });
+        return capitalizedLetters.join('');
+    });
+    
+  }
+
+  console.log(titleCased());
